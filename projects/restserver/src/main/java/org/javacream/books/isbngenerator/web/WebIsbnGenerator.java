0d30caf70d1eab171b8c44561f3f1f07cfac2e1f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-10 13:21:15 +0100

    introduce micrometer

--- projects/restserver/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
+++ projects/restserver/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+
 @RestController
 @RequestMapping(path = "api")
 @CrossOrigin
 public class WebIsbnGenerator {
 	@Autowired
 	IsbnGenerator isbnGenerator;
-
+	@Timed(value = "isbngenerator.next.time", description = "Time taken to create an isbn")
 	@PostMapping(path = "isbn", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String nextIsbn() {
 		return isbnGenerator.next();
